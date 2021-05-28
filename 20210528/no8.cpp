1820bc90db12ff2949facdd1c7c072fef2cbca09
devcui <devcui@outlook.com>
2021-05-29 00:02:32 +0800

    :sparkles: no8
    add fibs no8

--- 20210528/no8.cpp
+++ 20210528/no8.cpp
@@ -2,7 +2,7 @@
  * @Author: cuihaonan
  * @Email: devcui@outlook.com
  * @Date: 2021-05-28 17:53:24
- * @LastEditTime: 2021-05-28 22:53:45
+ * @LastEditTime: 2021-05-29 00:01:45
  * @LastEditors: cuihaonan
  * @Description:  有一分数序列，2/1,3/2,5/3,8/5，计算该序列的前n项和，n由键盘输入。
  * @FilePath: /homeworks/20210528/no8.cpp
@@ -14,28 +14,27 @@
 
 using namespace std;
 
-int fib(int n, vector<int> arr)
-{
-    if (n <= 0)
-    {
-        return 0;
-    }
-    else if (n == 1)
-    {
-
-        return 1;
-    }
-    else
-    {
-        arr.push_back(fib(n - 1, arr) + fib(n - 2, arr));
-    }
-}
-
 int main()
 {
-    vector<int> molecule;
-    vector<int> denominator;
-    fib(5, molecule);
-    cout << molecule << endl;
-    return 0;
+    int i;
+    cout << "Please input N" << endl;
+    int N;
+    cin >> N;
+    //fibs
+    vector<unsigned long int> arr(N + 2);
+    arr[0] = 1;
+    arr[1] = 1;
+    // 1,1,2,3,5,8,13
+    for (i = 2; i <= N; i++)
+    {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    long double sum = 0;
+    for (i = 2; i <= N; i++)
+    {
+        // 2/1+3/2+5/3
+        cout << arr[i] << "/" << arr[i - 1] << endl;
+        sum += ((long double)arr[i] / (long double)arr[i - 1]);
+    }
+    cout << sum << endl;
 }
